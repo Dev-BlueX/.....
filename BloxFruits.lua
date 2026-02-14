47891260ac171d09c256b66c11e5241cc83ba1cc
Dev-BlueX <bluexscript11@gmail.com>
2026-02-15 06:25:30 +0700

    Update loader URL in BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2ad923d991432294523f3d055275c8a6.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9b00c7743a97f0e815b4bbfe5fd9dc8b.lua"))()
